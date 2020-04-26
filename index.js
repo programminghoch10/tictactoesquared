ef45ba8e41c77993fa303913687a35f3afc5b8c5
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-04-26 19:59:37 +0200

    added redirect for index.html
    to ensure the use of the node server

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var app = express();  
+var express = require('express')
+var app = express()
 let fs = require('fs')
 
 const DIR = "./docs/"
 
+app.get('/index.html', function(req, res) {
+    res.redirect('/')
+})
+
 app.use(express.static(DIR))
 
 app.get('/', function(req, res) {
-    res.render(DIR + 'index.html');
-});
+    res.render(DIR + 'index.html')
+})
 
 app.listen(5500, '127.0.0.1')
