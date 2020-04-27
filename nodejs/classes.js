4a81fe71b24f216c4054541a0302ec0adbc9b184
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-04-27 15:57:35 +0200

    created classes User and Lobby

---
+++ nodejs/classes.js
@@ -0,0 +1,37 @@
+class User {
+  constructor() {
+    this.id = 0;
+    this.token = "";
+    this.humanid = "";
+    this.name = "";
+    this.creationtime = 0;
+    this.lastacttime = 0;
+    this.lobbytokens = "";
+    this.lobbyinvitetokens = "";
+  }
+
+}
+
+class Lobby {
+  constructor() {
+    this.id = 0;
+    this.token = "";
+    this.humanid = "";
+    this.game = "";
+    this.name = "";
+    this.description = "";
+    this.password = "";
+    this.privacy = "";
+    this.creationtime = 0;
+    this.lastacttime = 0;
+    this.timeout = 0;
+    this.usertokens = "";
+    this.userinvitetokens = "";
+  }
+
+}
+
+module.exports = {
+    User: User,
+    Lobby: Lobby
+}
