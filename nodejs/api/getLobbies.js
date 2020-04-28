8bca8bf6e60016e4ed6bc57ce0cd131494c4da81
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-04-28 18:03:10 +0200

    added filters to getLobbies.js

--- nodejs/api/getLobbies.js
+++ nodejs/api/getLobbies.js
@@ -7,15 +7,60 @@ const sql = require('../sql.js')
 
 router.post('/api/getLobbies', async function(req, res) {
 
-    let lobbies = await sql.getLobbies()
+    let privacyFilter = req.body.privacy
+    let fieldSizeFilter = req.body.fieldSize
+    let nameFilter = req.body.name
+    let hasPasswordFilter = req.body.hasPassword
+    let usertokenFilter = req.body.usertoken
 
+    let lobbies = await sql.getLobbies()
     if (!lobbies) {
         res.sendStatus(500)
         return
     }
     
+    if (!common.isStringEmpty(privacyFilter)) {
+        lobbies = lobbies.filter(function(lobby) {return lobby.privacy == privacyFilter})
+    }
+
+    if (fieldSizeFilter != null) {
+        lobbies = lobbies.filter(function(lobby) {
+            return lobby.game.substring(0, lobby.game.indexOf("-")) == fieldSizeFilter
+        })
+    }
+
+    if (!common.isStringEmpty(nameFilter)) {
+        lobbies = lobbies.filter(function(lobby) {return lobby.name == nameFilter})
+    }
+
+    if (hasPasswordFilter != null) {
+        lobbies = lobbies.filter(function(lobby) {
+            return hasPasswordFilter == !common.isStringEmpty(lobby.password)
+        })
+    }
+
+    if (!common.isStringEmpty(usertokenFilter)) {
+        lobbies = lobbies.filter(function(lobby) {
+            let containsUserToken = false
+            for (let i = 0; i < lobby.correlations.length; i++) {
+                const correlation = lobby.correlations[i]
+                if (correlation.usertoken == usertokenFilter) {
+                    containsUserToken = true
+                    break
+                }
+            }
+            return containsUserToken
+        })
+    }
+
     for (let i = 0; i < lobbies.length; i++) {
         lobbies[i].password = ""
+    }
+
+    if (lobbies.length == 0) {
+        res.status(204)
+    } else {
+        res.status(200)
     }
 
     res.send(JSON.stringify(lobbies))
