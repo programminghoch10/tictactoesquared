02606cb904cef18c18e539c34f35322cc853dfc5
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-04-30 16:25:53 +0200

    added limit on how many lobbys a user can be in

--- nodejs/common.js
+++ nodejs/common.js
@@ -1,6 +1,9 @@
 let crypto = require("crypto")
 
+const USER_LIMIT = 50 //limit on how many lobbies a user can be in
+
 module.exports = {
+  USER_LIMIT: USER_LIMIT,
   hash: (value) => {
     let hash = crypto.createHash("sha256")
     hash.update(value)
