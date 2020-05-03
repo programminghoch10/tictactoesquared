61eeb35dcdb1c5860c15951d8948e8d616db6c42
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2020-05-03 16:59:08 +0200

    added a different timeout for quickgame lobbies

--- nodejs/cleanup.js
+++ nodejs/cleanup.js
@@ -4,6 +4,7 @@ const common = require('./common.js')
 
 const GAME_FINISHED_TIMEOUT = 60 * 15 //in seconds, how long a lobby containing a finished game exists before getting deleted
 const LOBBY_LEFT_TIMEOUT = 60 * 60 * 24 //in seconds, how long until a lobby, where the opponent left, gets deleted
+const LOBBY_QUICKGAME_TIMEOUT = 60 * 60 * 24 * 7 //in seconds, how long until a quick game lobby gets deleted due to inactivity
 
 async function cleanup() {
   console.log("Performing cleanup")
@@ -21,6 +22,7 @@ async function cleanup() {
       }
       deleteThisLobby += ((lobby.game.indexOf("!") > -1) && (lobby.lastacttime + GAME_FINISHED_TIMEOUT <= now))
       deleteThisLobby += (lobby.hasFlag("left") && (lobby.lastacttime + LOBBY_LEFT_TIMEOUT <= now))
+      deleteThisLobby += (lobby.hasFlag("quickgame") && (lobby.lastacttime + LOBBY_QUICKGAME_TIMEOUT <= now))
       return (deleteThisLobby > 0)
     })
     lobbies.forEach(lobby => {
